fead3d064de63db7c1c1368c22d35a2d7db5e122
Awareqwx <benpeters97@yahoo.com>
2017-10-13 14:36:44 -0700

    Initial Commit

---
+++ server.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, redirect, render_template, session, flash
+from mysqlconnection import MySQLConnector
+app = Flask(__name__)
+mysql = MySQLConnector(app,'allfriendsdb')
+@app.route('/')
+def index():
+    query = "SELECT * FROM friends"
+    friends = mysql.query_db(query)
+    return render_template('index.html', all_friends=friends)
+@app.route('/addfriend', methods=['POST'])
+def friends():
+    query = "INSERT INTO friends (name, age, created_at, updated_at) VALUES (:name, :age, NOW(), NOW())"
+    data = {
+             'name': request.form['name'],
+             'age': request.form['age']
+           }
+    mysql.query_db(query, data)
+    return redirect('/')
+app.run(debug=True)
